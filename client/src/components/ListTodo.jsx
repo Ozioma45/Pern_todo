9fbb850980bea48e9d40b572bf483bc17a05e8b2
Ozioma45 <oziomaegole@gmail.com>
2025-01-30 14:53:18 +0100

    add functionality to delete todo

--- client/src/components/ListTodo.jsx
+++ client/src/components/ListTodo.jsx
@@ -3,6 +3,19 @@ import React, { Fragment, useEffect, useState } from "react";
 const ListTodo = () => {
   const [todos, setTodos] = useState([]);
 
+  //delete function
+  const deleteTodos = async (id) => {
+    try {
+      const deleteTodos = await fetch(`http://localhost:5000/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -37,7 +50,11 @@ const ListTodo = () => {
                 <button className="btn btn-warning">Edit</button>
               </td>
               <td>
-                <button className="btn btn-danger">Delete</button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodos(todo.todo_id)}>
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
