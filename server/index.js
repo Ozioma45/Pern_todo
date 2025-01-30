34c9c9e79bb7217f9238aad83e18991e06300152
Ozioma45 <oziomaegole@gmail.com>
2025-01-30 12:40:27 +0100

    added route to delete, update and get a todo

--- server/index.js
+++ server/index.js
@@ -45,6 +45,50 @@ app.get("/todos", async (req, res) => {
   }
 });
 
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+    console.log(req.params);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated successfully");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // Start the server
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
